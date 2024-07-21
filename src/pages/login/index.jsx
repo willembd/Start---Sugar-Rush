703cf781d57d03c5b462dacca89491392c3ce1d6
LuizFloriano <luizfloriano92@gmail.com>
2024-07-21 19:00:47 -0300

    Reapply "Squashed commit of the following:"
    This reverts commit e94bcdf51aa715004c79cd3dd451ee0d3b8227ec.

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,27 @@
+import Fundo from "./img/fundo.png"
+import { mainLogin } from "./style"
+
 export default function Login(){
     return(
         <>
-            <h1>Login</h1>
+        <mainLogin>
+            <div className="cardFundo">
+                <img src={Fundo} alt="Fundo" />
+                <div className="card">
+                    <section>
+                        <h1>Login</h1>
+
+                        <h5>E-mail</h5>
+                        <h5>Senha</h5>
+                    </section>
+
+                    <button>Entrar</button>
+
+                    <p>Não possui conta? <span> CRIE JÁ!</span></p>
+                </div>
+            </div>
+        </mainLogin>
+          
         </>
     )
 }
