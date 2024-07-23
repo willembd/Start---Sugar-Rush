d64408ff1661efe14fbfdab39d969bd3b96ca1f9
RuteCajueiro <ruthynhas344@gmail.com>
2024-07-22 21:54:17 -0300

    feat: banner

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
 import { useNavigate } from "react-router-dom"
-
+import { Secao, LogoSection, DonutSection } from "./style.js";
 import { Navbar } from "../../componentes/navbar";
 import { HomeContainer } from './style'
 import { Button } from "../../componentes/button";
+import LogoSugar from "../../assets/Sugar Rush.png"
+import LogoDonuts from "../../assets/Donuts.png"
+
+
 
 export default function Home(){
     const navigation = useNavigate();
@@ -12,6 +16,7 @@ export default function Home(){
     }
 
     return(
+        <div>
        <HomeContainer>
             <Navbar navButton={ 
                 <Button 
@@ -20,6 +25,26 @@ export default function Home(){
                     onClick={onLoginClick} 
                 />}
             />
+                 <Secao>
+                    <LogoSection>
+                        <img src={LogoSugar} alt="Sugar-rush" />
+                        <p>Doçura em alta velocidade</p>
+                    </LogoSection>
+                    <DonutSection>
+
+                        <img 
+                            src={LogoDonuts} 
+                            alt="LogoDonuts"  
+                        /> 
+                    </DonutSection>
+
+                </Secao>
        </HomeContainer>
+
+       
+     
+   </div>
     )
+
+   
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,5 +1,49 @@
 import styled from 'styled-components'
 
+
 export const HomeContainer = styled.div`
-    
-`
+    /* Seus estilos */
+`;
+
+export const Secao = styled.section`
+    background-color: #F2C3CB;
+    margin-top: 2px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+
+    p {
+        font-size: 20px;
+        color: #5E2806;
+        margin-top: 20px; 
+    }
+`;
+
+export const LogoSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    text-align: center;
+    margin-left: 147px;
+    margin-top:100px;
+    margin-right: 20px;
+    img {
+        max-width: 100%;
+    }
+
+    p {
+        margin-top: 20px; 
+        font-size: 32px;
+        color: #5E2806;
+        text-shadow: -2px 1px 1px white;
+    }
+`;
+
+export const DonutSection = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    img {
+        max-width: 100%;
+    }
+`;
